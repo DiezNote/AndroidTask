8791aa8642dbf23b90a84120e337f3559dba86c5
DiezNote <dieznote@gmail.com>
2017-07-17 02:50:00 +0300

    Переносит на второе активити

--- app/src/main/java/com/dieznote/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/dieznote/myfirstapplication/MainActivity.java
@@ -1,19 +1,36 @@
 package com.dieznote.myfirstapplication;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+    Button buttonOtpr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        buttonOtpr = (Button) findViewById(R.id.buttonOtpr);
+        buttonOtpr.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.buttonOtpr:
+                Intent intent = new Intent(this,Second.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {

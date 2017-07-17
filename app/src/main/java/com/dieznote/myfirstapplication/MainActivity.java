c44f38d1038f25cd9283cb4a08621ff34341a753
DiezNote <dieznote@gmail.com>
2017-07-17 04:24:52 +0300

    Переносит на второе активити

--- app/src/main/java/com/dieznote/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/dieznote/myfirstapplication/MainActivity.java
@@ -1,16 +1,22 @@
 package com.dieznote.myfirstapplication;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
     Button buttonOtpr;
+    Button buttonClean;
+    EditText editText;
+    final String TAG = "lifecycle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +25,38 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
         buttonOtpr = (Button) findViewById(R.id.buttonOtpr);
         buttonOtpr.setOnClickListener(this);
+        buttonClean = (Button) findViewById(R.id.buttonClean);
+        buttonClean.setOnClickListener(this);
+        editText = (EditText) findViewById(R.id.editText);
+        Log.d(TAG, "MainActivity onCreate");
     }
     @Override
     public void onClick(View v){
-        switch (v.getId()){
+        Intent intent = new Intent(this,Second.class);
+        switch (v.getId()) {
             case R.id.buttonOtpr:
-                Intent intent = new Intent(this,Second.class);
-                startActivity(intent);
+                intent.putExtra("text",editText.getText().toString());
+                startActivityForResult(intent, 1);
                 break;
-            default:
+            case R.id.buttonClean:
+                editText.setText("");
                 break;
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(data == null){
+            return;
+        }String nettext = data.getStringExtra("n");
+        editText.setText(nettext);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"MainActivity onRestart");
     }
 
     @Override
@@ -52,5 +79,29 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "MainActivity onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "MainActivity onPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "MainActivity onDestroy");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "MainActivity onStop");
     }
 }
--- app/src/main/java/com/dieznote/myfirstapplication/Second.java
+++ app/src/main/java/com/dieznote/myfirstapplication/Second.java
@@ -1,17 +1,56 @@
 package com.dieznote.myfirstapplication;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
-public class Second extends ActionBarActivity {
+public class Second extends ActionBarActivity implements View.OnClickListener{
+    Button buttonOk;
+    Button buttonOtklonit;
+    TextView textView;
+    final String TAG = "lifecycle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        buttonOk = (Button) findViewById(R.id.buttonOk);
+        buttonOk.setOnClickListener(this);
+
+        buttonOtklonit = (Button) findViewById(R.id.buttonNet);
+        buttonOtklonit.setOnClickListener(this);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        Intent intent  = getIntent();
+        String text = intent.getStringExtra("text");
+        textView.setText(text);
+        Log.d(TAG, "Second onCreate");
+
+    }
+    @Override
+    public void onClick(View v){
+        Intent intent = new Intent();
+        switch (v.getId()) {
+            case R.id.buttonOk:
+                intent.putExtra("n","");
+                break;
+            case R.id.buttonNet:
+                intent.putExtra("n",textView.getText().toString());
+                break;
+        }
+            setResult(RESULT_OK,intent);
+            finish();
     }
 
 
@@ -36,4 +75,33 @@ public class Second extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "Second onRestart");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "Second onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "Second onPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "Second onDestroy");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "Second onStop");
+    }
 }
+
